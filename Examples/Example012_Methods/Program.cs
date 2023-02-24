2b5cb6e9fc1fba1898f85928db78603f2154df42
Vadim Cheremnykh <chere96@yandex.ru>
2023-02-24 19:38:10 +0400

    changed Example012

--- Examples/Example012_Methods/Program.cs
+++ Examples/Example012_Methods/Program.cs
@@ -46,3 +46,12 @@
 // }
 // string res = Method4(10, "Текст");
 // Console.WriteLine(res);
+
+for (int i = 2; i <= 10; i++)
+{
+    for(int j =2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
